c3bd7747cdb246548fcf1e1670f6d2e3451d4a50
Jakub Delicat <jakub.delicat@husarion.com>
2022-07-20 11:16:28 +0200

    Publishing map without offset

--- src/floor_heat_mapper/include/floor_heat_mapper/map_publisher.hpp
+++ src/floor_heat_mapper/include/floor_heat_mapper/map_publisher.hpp
@@ -1,31 +1,61 @@
 #pragma once
 #include <tf2/LinearMath/Quaternion.h>
+#include <tf2/exceptions.h>
+#include <tf2_ros/buffer.h>
 #include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <chrono>
+#include <cmath>
 #include <functional>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <memory>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <vector>
 
 namespace floor_heat_mapper {
 
 constexpr char NODE_NAME[] = "Map_Publisher";
 constexpr char MAP_TOPIC_NAME[] = "floor_heatmap";
-constexpr char MAP_FRAME_NAME[] = "floor_heatmap";
+constexpr char MAP_FRAME_NAME[] = "map";
+constexpr char ODOM_FRAME_NAME[] = "odom";
+constexpr char BASE_LINK_FRAME_NAME[] = "base_link";
+
+constexpr char HEATMAP_FRAME_NAME[] = "floor_heatmap_frame";
+constexpr char THERMAL_CAMERA_FRAME_NAME[] = "thermal_camera_frame";
+
+constexpr float THERMAL_CAMERA_X_FOV_HALF_ANGLE = 0.9599310885968813;
+constexpr float THERMAL_CAMERA_Y_FOV_HALF_ANGLE = 0.6544984694978736;
+
+constexpr float THERMAL_CAMERA_FRAME_X = 0.225;
+constexpr float THERMAL_CAMERA_FRAME_Z = 0.178;
+
+constexpr uint8_t IMAGE_WIDTH = 16;
+constexpr uint8_t IMAGE_HEIGHT = 8;
 
 class MapPublisher : public rclcpp::Node {
    public:
     MapPublisher();
 
    private:
+    void initialize_heatmap_origin_from_base_link();
+    void create_heatmap_to_map_tf();
+    void create_thermal_camera_to_base_link_tf();
+    void create_heatmap_settings();
     void timer_callback();
+
     rclcpp::TimerBase::SharedPtr timer_;
-    nav_msgs::msg::OccupancyGrid map_msg_;
-    std::vector<int8_t> map_data_;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
-    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_pub_;
+    nav_msgs::msg::OccupancyGrid heatmap_msg_;
+    std::vector<int8_t> heatmap_data_;
+
+    std::shared_ptr<tf2_ros::TransformListener> transform_listener_{nullptr};
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+
+
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr heatmap_pub_;
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_pub_;
 };
 }  // namespace floor_heat_mapper
--- src/floor_heat_mapper/src/map_publisher.cpp
+++ src/floor_heat_mapper/src/map_publisher.cpp
@@ -3,36 +3,77 @@
 namespace floor_heat_mapper {
 MapPublisher::MapPublisher()
     : rclcpp::Node(NODE_NAME) {
-    map_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(MAP_TOPIC_NAME, 10);
-    tf_pub_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+    heatmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(MAP_TOPIC_NAME, 10);
+    static_tf_pub_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
     using namespace std::chrono_literals;
-    timer_ = this->create_wall_timer(
-        1000ms, std::bind(&MapPublisher::timer_callback, this));
+    timer_ = this->create_wall_timer(100ms, std::bind(&MapPublisher::timer_callback, this));
 
-    map_msg_.info.resolution = 0.1;
-    map_msg_.info.width = 16;
-    map_msg_.info.height = 12;
-    map_msg_.data.resize(map_msg_.info.width * map_msg_.info.height);
-    RCLCPP_INFO(get_logger(), "Map Publisher, ready to publish.");
-    for (auto i = 0u; i < 12 * 16; ++i){
-        map_msg_.data[i] = -1;
-    }
 
-    geometry_msgs::msg::TransformStamped t;
-    t.header.stamp = now();
-    t.header.frame_id = MAP_FRAME_NAME;
-    t.child_frame_id = "map";
-    tf_pub_->sendTransform(t);
+    create_heatmap_to_map_tf();
+    create_heatmap_settings();
+    create_thermal_camera_to_base_link_tf();
+    RCLCPP_INFO(get_logger(), "Map constructed!");
 }
 
-void MapPublisher::timer_callback(){
-    for (auto i = 0u; i < 12 * 8; ++i) {
-        map_msg_.data[i] = i;
-    }
-    map_msg_.header.stamp = now();
-    map_msg_.header.frame_id = MAP_FRAME_NAME;
+void MapPublisher::initialize_heatmap_origin_from_base_link() {
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(get_clock());
+    transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    geometry_msgs::msg::TransformStamped map_to_thermal_camera_tf;
 
-    map_pub_->publish(map_msg_);
+    bool found_transformation = false;
+    do {
+        try {
+            map_to_thermal_camera_tf = tf_buffer_->lookupTransform(MAP_FRAME_NAME, THERMAL_CAMERA_FRAME_NAME, tf2::TimePointZero);
+            found_transformation = true;
+        } catch (tf2::TransformException& ex) {
+            RCLCPP_INFO(get_logger(), "Could not find transform base_link to map: %s", ex.what());
+        }
+    } while (not found_transformation);
+
+    heatmap_msg_.info.origin.position.x = map_to_thermal_camera_tf.transform.translation.x;
+    heatmap_msg_.info.origin.position.y = map_to_thermal_camera_tf.transform.translation.y;
+    heatmap_msg_.info.origin.orientation = map_to_thermal_camera_tf.transform.rotation;
+
+    tf2::Quaternion q_orig, q_rot, q_new;
+    tf2::convert(map_to_thermal_camera_tf.transform.rotation, q_orig);
+    q_rot.setRPY(0.0, 0.0, -M_PI / 2);
+    q_new = q_rot * q_orig;
+    q_new.normalize();
+    heatmap_msg_.info.origin.orientation = tf2::toMsg(q_new);
+}
+
+void MapPublisher::create_thermal_camera_to_base_link_tf() {
+    geometry_msgs::msg::TransformStamped t;
+    t.header.stamp = now();
+    t.header.frame_id = BASE_LINK_FRAME_NAME;
+    t.child_frame_id = THERMAL_CAMERA_FRAME_NAME;
+    t.transform.translation.x = THERMAL_CAMERA_FRAME_X;
+    t.transform.translation.z = THERMAL_CAMERA_FRAME_Z;
+    static_tf_pub_->sendTransform(t);
+}
+
+void MapPublisher::create_heatmap_to_map_tf() {
+    geometry_msgs::msg::TransformStamped t;
+    t.header.stamp = now();
+    t.header.frame_id = HEATMAP_FRAME_NAME;
+    t.child_frame_id = MAP_FRAME_NAME;
+    static_tf_pub_->sendTransform(t);
+}
+
+void MapPublisher::create_heatmap_settings() {
+    heatmap_msg_.info.resolution = tan(THERMAL_CAMERA_X_FOV_HALF_ANGLE) * THERMAL_CAMERA_FRAME_Z / 2 / IMAGE_WIDTH;
+    heatmap_msg_.info.width = IMAGE_WIDTH;
+    heatmap_msg_.info.height = IMAGE_HEIGHT;
+    heatmap_msg_.data.resize(heatmap_msg_.info.width * heatmap_msg_.info.height);
+
+    initialize_heatmap_origin_from_base_link();
+    RCLCPP_INFO(get_logger(), "Map Publisher, ready to publish. Resolution: %f", heatmap_msg_.info.resolution);
+}
+
+void MapPublisher::timer_callback() {
+    heatmap_msg_.header.stamp = now();
+    heatmap_msg_.header.frame_id = HEATMAP_FRAME_NAME;
+    heatmap_pub_->publish(heatmap_msg_);
 }
 }  // namespace floor_heat_mapper
