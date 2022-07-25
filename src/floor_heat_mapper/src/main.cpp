c6a40c143b6f42a756cad3d8a7eebb6f3376a166
Jakub Delicat <jakub.delicat@husarion.com>
2022-07-25 12:49:30 +0200

    Removed old source

--- src/floor_heat_mapper/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "floor_heat_mapper/heatmap_publisher.hpp"
-#include <rclcpp/rclcpp.hpp>
-
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<floor_heat_mapper::HeatmapPublisher>());
-    rclcpp::shutdown();
-    return 0;
-}
